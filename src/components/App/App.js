d6975f51875b2bbe3a5603bfdd0f6ff5dc949be8
Caleb Hatch <hatchcaleb@gmail.com>
2021-12-09 13:54:20 -0600

    fixing merge issues

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
 import {HashRouter as Router, Route, Link} from 'react-router-dom';
 
-import SearchView from './searchView/searchView';
-import FavoriteView from './favoriteView/favoriteView';
+
 
 function App(props) {
   return (
     <div>
-      <h1>Giphy Search!</h1>
       
-      <Router>
-        <p><Link to='/'>Search</Link></p>
-        <p><Link to='/favorite'>Favorites</Link></p>
-
-        <Route exact path="/">
-          <SearchView />
-        </Route>
-
-        <Route exact path="/favorite">
-          <FavoriteView />
-        </Route>
-      </Router>
     </div>
   );
 }
